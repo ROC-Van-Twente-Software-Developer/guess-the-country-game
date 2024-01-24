d7e8523cfe6a7273669772504e016329ef242b4e
Nikita Skliarov <nikitaskliarov2004@gmail.com>
2024-01-24 22:00:29 +0100

    Make code more readable, add some text if there are no game info yet

--- php/back-end/gameHistoryTableFunction.php
+++ php/back-end/gameHistoryTableFunction.php
@@ -1,26 +1,31 @@
 <?php
 
-$gamesHistory = getGamesHistory($db);
 function makeTable($fetchedData)
 {
-    foreach ($fetchedData as $row) {
-        echo "<tr>";
-        foreach ($row as $key => $cell) {
-            if ($key == 'date') {
-                $dateTimeObject = new DateTime($cell);
-                $dateArray = array(
-                    'year' => $dateTimeObject->format('Y'),
-                    'month' => $dateTimeObject->format('F'),
-                    'day' => $dateTimeObject->format('d'),
-                    'hours' => $dateTimeObject->format('H'),
-                    'minutes' => $dateTimeObject->format('i')
-                );
-                echo "<td class='p-3'>" . $dateArray['day'] . " " . $dateArray['month'] . " " . $dateArray['year'] . "," . $dateArray['hours'] . ":" . $dateArray['minutes'];
-            } else {
-                echo "<td class='p-3'>" . $cell . "</td>";
+    if ($fetchedData == null) {
+        echo "<tr><td> <h5 class='text-center'> There's nothing right now. 
+        Just play to add your game result here!</h5></td></tr>";
+    } else {
+        foreach ($fetchedData as $row) {
+            echo "<tr>";
+            foreach ($row as $key => $cell) {
+                if ($key == 'date') {
+                    $dateTimeObject = new DateTime($cell);
+                    $dateArray = array(
+                        'year' => $dateTimeObject->format('Y'),
+                        'month' => $dateTimeObject->format('F'),
+                        'day' => $dateTimeObject->format('d'),
+                        'hours' => $dateTimeObject->format('H'),
+                        'minutes' => $dateTimeObject->format('i')
+                    );
+                    echo "<td class='p-3'>" . $dateArray['day'] . " " . $dateArray['month']
+                        . " " . $dateArray['year'] . ", <strong>" . $dateArray['hours'] . ":" . $dateArray['minutes']
+                        . "</strong>";
+                } else {
+                    echo "<td class='p-3'>" . $cell . "</td>";
+                }
             }
+            echo "</tr>";
         }
-        echo "</tr>";
     }
-
 }

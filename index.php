ed0f9df91596ffc742c134c88349d1cd355673f2
Nikita Skliarov <nikitaskliarov2004@gmail.com>
2024-01-24 16:00:24 +0100

    added html table of games history, changed some positions of elements in page

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
+<?php
+include "php/db/tableQuery.php";
+include "php/back-end/gameHistoryTableFunction.php";
+$gamesHistory = getGamesHistory($db);
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -7,26 +13,45 @@
     <title>Country game!</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/index.css">
     <style>
         @import url('https://fonts.googleapis.com/css2?family=Nunito:wght@200&display=swap');
     </style>
 </head>
 
 <body>
-    <div class="container">
+    <div class="container main-container d-flex align-items-center justify-content-center">
         <div class="row">
-            <div class="col-6 col-sm-12">
-                <div class="card mt-5 bg-dark text-white position-absolute top-50 start-50 translate-middle">
+            <div class="col-12 col-sm-6">
+                <div class="card mt-5 bg-dark text-white">
                     <div class="card-body p-3">
-                        <h5 class="card-title">Country guess game!</h5>
-                        <p class="card-text">You will have 3 seconds to prepare. To submit your answer - press enter or
-                            click on 'answer button'.</p>
-                        <p class="card-text">Timer will stop about 60 seconds after begin. </p>
+                        <h5 class="card-title mb-3">Choose time of round (default - 60sec):</h5>
+                        <div class="form-group form-inline">
+                            <input type="number" id="timer-count" placeholder="Seconds" class="form-control">
+                        </div>
+                        <p class="card-text"></p>
                         <a href="pages/game.php" class="btn btn-success">Play</a>
                     </div>
                 </div>
             </div>
-            <div class="col-6 col-sm-12"></div>
+            <div class="col-12 col-sm-6">
+                <div class="card mt-5 bg-dark text-white">
+                    <div class="card-body p-3">
+                        <h5 class="card-title">Last games:</h5>
+                        <table class="table-dark">
+                            <tr>
+                                <th scope="col" class="p-2">Game</th>
+                                <th scope="col" class="p-2">Score</th>
+                                <th scope="col" class="p-2">Date</th>
+                                <th scope="col" class="p-2">Round duration</th>
+                            </tr>
+                            <?php
+                            makeTable($gamesHistory);
+                            ?>
+                        </table>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"

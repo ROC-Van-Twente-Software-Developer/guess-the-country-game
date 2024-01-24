1554e88fd4a02a941353461134f7baa28d71f1be
Nikita Skliarov <nikitaskliarov2004@gmail.com>
2024-01-24 21:59:42 +0100

    Change amount of visible games Add function for best score table

--- php/db/tableQuery.php
+++ php/db/tableQuery.php
@@ -3,7 +3,19 @@ include "connection.php";
 
 function getGamesHistory($db)
 {
-    $stmt = $db->prepare("SELECT * FROM games_history");
+    $stmt = $db->prepare("SELECT * FROM games_history
+    ORDER BY number DESC
+    LIMIT 3");
+    $stmt->execute();
+    $fetchedResult = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    return $fetchedResult;
+}
+
+function getBestScore($db) {
+    $stmt = $db->prepare("SELECT *
+    FROM games_history
+    ORDER BY score DESC, roundtime DESC
+    LIMIT 3");
     $stmt->execute();
     $fetchedResult = $stmt->fetchAll(PDO::FETCH_ASSOC);
     return $fetchedResult;
